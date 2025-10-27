33e325ffdcda6cb446381ae53fad9754ddb333d1
Pablo H. F. Roscoche <pablo.dkz2@gmail.com>
2025-10-27 03:04:33 -0300

    chore: add jwt, class and database packages and rabbitmq config

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,14 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+  const appContext = await NestFactory.createApplicationContext(AppModule);
+  const configService = appContext.get(ConfigService);
+
+  const rabbitUrl = configService.get<string>('RABBITMQ_URL');
+
+  if (!rabbitUrl) {
+    throw new Error('Missing env variable RABBITMQ_URL');
+  }
+
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
       transport: Transport.RMQ,
       options: {
-        urls: ['amqp://admin:admin@localhost:5672'],
+        urls: [rabbitUrl],
         queue: 'auth_queue',
         queueOptions: {
           durable: false,
