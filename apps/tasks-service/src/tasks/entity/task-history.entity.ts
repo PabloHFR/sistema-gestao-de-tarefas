984da3520d712f272cf1a2fff80667b95a0b9a53
Pablo H. F. Roscoche <pablo.dkz2@gmail.com>
2025-10-30 22:22:25 -0300

    feat: add entities and migration for tasks, comments and task-history tables

---
+++ apps/tasks-service/src/tasks/entity/task-history.entity.ts
@@ -0,0 +1,61 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Task } from './tasks.entity';
+
+export enum HistoryAction {
+  CREATED = 'CREATED',
+  UPDATED = 'UPDATED',
+  STATUS_CHANGED = 'STATUS_CHANGED',
+  ASSIGNED = 'ASSIGNED',
+  UNASSIGNED = 'UNASSIGNED',
+  COMMENTED = 'COMMENTED',
+}
+
+@Entity('task_history')
+export class TaskHistory {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    type: 'enum',
+    enum: HistoryAction,
+  })
+  action: HistoryAction;
+
+  // ID do usuário que fez a alteração
+  @Column('uuid')
+  userId: string;
+
+  // Username do usuário que fez a altereção (Desnormalizado)
+  @Column()
+  username: string;
+
+  // Campo que foi alterado (ex: "status", "priority", "assignedTo")
+  @Column({ nullable: true })
+  field: string;
+
+  // Valor antigo
+  @Column('text', { nullable: true })
+  oldValue: string;
+
+  // Valor novo
+  @Column('text', { nullable: true })
+  newValue: string;
+
+  // Relacionamento com Task
+  @ManyToOne(() => Task, (task) => task.history, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'taskId' })
+  task: Task;
+
+  @Column('uuid')
+  taskId: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
